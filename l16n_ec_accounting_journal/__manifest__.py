1dc765e6618542df79f10279e0133732de8618cd
Freddy Sebastian Cárdenas Rosero <142456682+fcr-octupus@users.noreply.github.com>
2023-08-29 21:42:05 -0500

    primera capacitacion
    instalacion de ambiente

---
+++ l16n_ec_accounting_journal/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': '[Ecuador] Accounting Journal support',
+    'version': '16',
+    'depends': ['account_accountant','l10n_ec'],
+    'author': 'OctupusTech S.A',
+    'website': "https://www.octupustech.com/",
+    'description': '[Ecuador] Accounting Journal support',
+    'category': 'Specific Industry Applications',
+    'data': [
+
+        'views/accounting_journal.xml',
+
+    ],
+}
---
+++ taller_res_partner/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Personalizacion - Taller",
+    "version": "0.0.0.1",
+    "category": "Partner/Localization", 
+    "description": """Accounting chart and localization for Ecuador""",
+    "author": "Sebastian Cardenas",
+    "depends": [
+        "web",
+        "contacts",
+        "base"],
+    "data": [
+        "views/taller_res_partner.xml",
+    ],
+}
